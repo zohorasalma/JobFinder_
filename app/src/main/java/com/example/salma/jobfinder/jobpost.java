aeccea256b562b17623a04651bb8cebf25c7fb18
Fatema tuz Zohora Salma <zohorasalma@gmail.com>
2019-03-06 09:20:17 +0600

    Initial commit

---
+++ app/src/main/java/com/example/salma/jobfinder/jobpost.java
@@ -0,0 +1,13 @@
+package com.example.salma.jobfinder;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class jobpost extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_jobpost);
+    }
+}
